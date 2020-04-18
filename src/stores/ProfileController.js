04d7d7209fbf0aa2421bcb451c169152300c1c12
Shehryar Assad <xshehryar@gmail.com>
2020-04-17 20:07:01 -0700

    Add profile controller

---
+++ src/stores/ProfileController.js
@@ -0,0 +1,77 @@
+import { action } from "mobx";
+import * as firebase from "firebase";
+import { UserStore } from "./UserStore";
+
+class ProfileControllerImpl {
+  async doesProfileWithUsernameExist(username) {
+    const snapshot = await firebase
+      .database()
+      .ref("/")
+      .child("users")
+      .orderByChild("username")
+      .equalTo(username)
+      .once("value");
+
+    return snapshot.exists();
+  }
+
+  async assertUsernameExists(username) {
+    const doesUserExist = await this.doesProfileWithUsernameExist(username);
+
+    if (!doesUserExist) {
+      throw new Error("Unable to find username");
+    }
+  }
+
+  @action
+  async sendFriendRequestToUsername(friendUsername) {
+    await this.assertUsernameExists(friendUsername);
+
+    firebase
+      .database()
+      .ref(`users/${UserStore.username}/friendRequests/`)
+      .push({
+        username: friendUsername,
+      });
+  }
+
+  async addFriend(friendUsername) {
+    await this.assertUsernameExists(friendUsername);
+
+    await firebase
+      .database()
+      .ref("/")
+      .child(`users/${UserStore.username}/`)
+      .child("friendRequests")
+      .orderByChild("username")
+      .equalTo(friendUsername)
+      .once("value", (snapshot) =>
+        snapshot.forEach((child) => child.ref.remove())
+      );
+
+    await firebase.database().ref(`users/${UserStore.username}/friends/`).push({
+      username: friendUsername,
+    });
+
+    await firebase.database().ref(`users/${friendUsername}/friends/`).push({
+      username: UserStore.username,
+    });
+  }
+
+  async removeFriend(friendUsername) {
+    await this.assertUsernameExists(friendUsername);
+
+    await firebase
+      .database()
+      .ref("/")
+      .child(`users/${UserStore.username}/`)
+      .child("friends")
+      .orderByChild("username")
+      .equalTo(friendUsername)
+      .once("value", (snapshot) =>
+        snapshot.forEach((child) => child.ref.remove())
+      );
+  }
+}
+
+export const ProfileController = new ProfileControllerImpl();
