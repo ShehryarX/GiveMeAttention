514ae1c51fec7dea5fad0ffbaa79937cf21542da
Shehryar Assad <xshehryar@gmail.com>
2020-04-17 17:38:12 -0700

    Add observer flag to homepage

--- src/screens/home/HomePage.js
+++ src/screens/home/HomePage.js
@@ -3,7 +3,10 @@ import { Text } from "react-native";
 import { Button } from "../../common/Button";
 import { FormTextInput } from "../../common/FormTextInput";
 import { HomeStore } from "./state";
+import UserStore from "../../stores/UserStore";
+import { observer } from "mobx-react";
 
+@observer
 export class HomePage extends React.Component {
   state = new HomeStore();
   render() {
@@ -15,7 +18,7 @@ export class HomePage extends React.Component {
           value={this.state.friendusername}
           returnKeyType="done"
           onChangeText={this.state.handlefriendusernameChange}
-          placeholder={"Friends name"}
+          placeholder={"Friend's name"}
         />
         <Button label={"Add Friend"} onPress={this.state.AcceptFriendRequest} />
       </>
